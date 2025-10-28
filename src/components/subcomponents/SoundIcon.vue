79aca86f34c14a04e073084b5daf22e998bc24f5
Gordon Lim <limgordon@hotmail.com>
2025-10-28 04:44:54 +0000

    Ex1 Complete

--- src/components/subcomponents/SoundIcon.vue
+++ src/components/subcomponents/SoundIcon.vue
@@ -6,10 +6,13 @@
             }
         },
         // TODO: add code to define a necessary prop
+        props: [
+            "soundLvl"
+        ]
     }
 </script>
 
 <template>
     <!-- TODO: add your template code here. Use span element. --> 
-    
+    <span>{{ soundEmojis[Number(soundLvl)] }}</span>
 </template>
